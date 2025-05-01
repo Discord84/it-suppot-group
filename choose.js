5c10b58be54415b4b57532a6e7c5c45a0701fbb2
Discord84 <warawara2000top@gmail.com>
2025-05-01 11:27:23 +0300

    adaptive finished

--- choose.js
+++ choose.js
@@ -101,3 +101,20 @@ forma.addEventListener("submit",async (e)=>{
         alert ("ваши данные не были отправлены в пентагон по ошибке")
     }
 })
+
+const burger=document.querySelector(".burger")
+const burgerCont=document.querySelector(".navigation-cont")
+burger.addEventListener("click", function(){
+    burger.classList.toggle("burger-active")
+})
+burger.addEventListener("click", function(){
+    burgerCont.classList.toggle("navigation-cont-active")
+})
+const burger2=document.querySelector(".burger2")
+const burgerCont2=document.querySelector(".black-navigation-cont")
+burger2.addEventListener("click", function(){
+    burger2.classList.toggle("burger2-active")
+})
+burger2.addEventListener("click", function(){
+    burgerCont2.classList.toggle("black-navigation-cont-active")
+})
